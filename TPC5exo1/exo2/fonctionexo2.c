b7564a054f23a8d3d39072fe78f45ffac74cc6d7
Corentin VARLET <corentin.varlet@student.yncrea.fr>
2020-11-24 15:55:48 +0100

    avancement exo2

--- TPC5exo1/exo2/fonctionexo2.c
+++ TPC5exo1/exo2/fonctionexo2.c
@@ -14,7 +14,17 @@ Tableau newArray() {// creation d'un tableau vide
 	return tab;
 }
 
-int incrementArraySize(Tableau* tab, int incrementValue) {
-	if (tab->elt)
+int incrementArraySize(Tableau* tab, int incrementValue) {// vérifie et agrandit si possible le tableau
+	int* tmp = tab->elt;
+	tab->elt = (int*)realloc(tab->elt + incrementValue, sizeof(int));
+	if (tab->elt == NULL) {// on vérifie si l'adresse n'est pas vide
+		tab->elt = tmp;
+		return -1;
+	}
+	int compt = 0;
+	for (compt = 0; compt < incrementValue; compt++) {// on remplit les nouvelles cases du tableau
+		tab->elt[tab->size + compt] = 0;
+	}
+	return tab->size + incrementValue ; // on renvoie la nouvelle taille du tableau
 
 }
